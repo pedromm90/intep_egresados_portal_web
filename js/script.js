c8443078caae8ec7999d75566b9a9cb647a668ba
pedromm90 <pmunem1990@gmail.com>
2025-06-11 21:25:31 -0500

    INTEP Egresados - Portal web

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ document.getElementById("agregarInput").addEventListener("click", function() {
   const nuevoInput = document.createElement("input");
   nuevoInput.type = "text";
   nuevoInput.className = "form-control";
-  nuevoInput.id = "floatingInput";
+  // nuevoInput.id = "floatingInput";
   nuevoInput.placeholder = "URL de tu perfil.";
 
   // Crear el botón de eliminación
   const botonEliminar = document.createElement("button");
   const iconoEliminar = document.createElement("span");
+  botonEliminar.className = "borrar-redes";
+  // botonEliminar.className = "mt-2";
   iconoEliminar.textContent = "delete";
   iconoEliminar.className = "material-icons";
   botonEliminar.onclick = function() {
